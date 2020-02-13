cfc7d3e218b76fb148a826af5eacaa646513347f
chris-kwl <github_g70b545@ext.kwl-mail.de>
2020-02-13 13:49:26 +0100

    improvements and one-time-Skript

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import tempfile
 import argparse
 import sys
 import numpy as np
-from aiocron import crontab
+# from aiocron import crontab
 from pyppeteer import launch
 from PIL import Image
+import PIL.ImageOps
 
 # TODO fix ignored KeyboardInterrupt when running in the the event loop (run_forever)
 # TODO implement proper reset and shutdown of the screen when the systemd service is stopped or the raspberry pi is shut down
@@ -21,11 +22,12 @@ import epd7in5_V2
 
 
 # Global config
-display_width = 480		# Width of the display
-display_height = 800		# Height of the display
-is_portrait = True		# True of the display should be in landscape mode (make sure to adjust the width and height accordingly)
+display_width = 800		# Width of the display
+display_height = 480		# Height of the display
+is_portrait = False		# True of the display should be in landscape mode (make sure to adjust the width and height accordingly)
+is_topdown = True
 wait_to_load = 60		# Page load timeout
-wait_after_load = 18		# Time to evaluate the JS afte the page load (f.e. to lazy-load the calendar data)
+wait_after_load = 30		# Time to evaluate the JS afte the page load (f.e. to lazy-load the calendar data) default=18
 url = 'http://localhost:8080'	# URL to create the screenshot of
 
 def reset_screen():
@@ -85,10 +87,14 @@ async def refresh():
         image = Image.open(tmp_file)
         # Replace all colors with are neither black nor red with white
         image = remove_aliasing_artefacts(image)
+        image = PIL.ImageOps.invert(image)
         # Rotate the image by 90°
         if is_portrait:
            logging.debug('Rotating image (portrait mode).')
            image = image.rotate(90)
+        if is_topdown:
+           logging.debug('Rotating image (topdown mode).')
+           image = image.rotate(180)
         logging.debug('Sending image to screen.')
         # epd.display_frame(epd.get_frame_buffer(image))
         epd.display(epd.getbuffer(image))
@@ -113,7 +119,7 @@ def main():
         if not args.reset:
             if args.cron:
                 logging.info(f'Scheduling the refresh using the schedule "{args.cron}".')
-                crontab(args.cron, func=refresh)
+                # crontab(args.cron, func=refresh)
                 # Initially refresh the display before relying on the schedule
                 asyncio.get_event_loop().run_until_complete(refresh())
                 asyncio.get_event_loop().run_forever()
@@ -124,7 +130,7 @@ def main():
         logging.info('Shutting down after receiving a keyboard interrupt.')
     finally:
         logging.info('Resetting screen.')
-        reset_screen()
+        # reset_screen()
 
 
 if __name__ == '__main__':
